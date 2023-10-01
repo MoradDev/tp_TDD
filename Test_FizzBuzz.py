02dc020b4208a432385809a501b6e5098ba49a96
MoradDev <morad.mlik@protonmail.com>
2023-10-01 19:03:21 +0200

    creation 2eme test qui fail sous forme de subTest

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -7,8 +7,11 @@ class TestFizzBuzz (unittest.TestCase) :
         self.instance=FizzBuzz()
 
     def test_affiche(self) :
-        self.assertEqual(self.instance.affiche(), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz373839Buzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374Buzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz")
-
+        with self.subTest(self) :
+            self.assertEqual(self.instance.affiche(100), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz373839Buzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374Buzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz")
+        with self.subTest(self) :
+            self.assertEqual(self.instance.affiche(15), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee")
+    
     def tearDown(self) :
         pass
 
