c838db6acfebe0ae61bc9984dddd2b04e78aaab7
MoradDev <morad.mlik@protonmail.com>
2023-10-01 19:03:35 +0200

    Pareil

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,5 +2,5 @@
 
 class FizzBuzz :
 
-    def affiche (self) :
+    def affiche (self, nombre) :
         return "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz373839Buzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374Buzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz"
