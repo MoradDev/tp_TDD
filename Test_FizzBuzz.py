7165981b4a8a03795e02c9c801bb7be5c3614313
MoradDev <morad.mlik@protonmail.com>
2023-10-01 18:53:29 +0200

    ajout tearDown vide et unittest.main()

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -5,5 +5,12 @@ class TestFizzBuzz (unittest.TestCase) :
     def setUp(self) :
         self.instance=FizzBuzz()
 
-    def test_affiche(self):
+    def test_affiche(self) :
         self.assertEqual(self.instance.affiche(), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz373839Buzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374Buzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz")
+
+    def tearDown(self) :
+        pass
+
+
+if __name__=='__main__' :
+    unittest.main()
