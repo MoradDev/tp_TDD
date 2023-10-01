4dcf5c7a2a58af9ea29a396f7df24ab81bdaaab7
MoradDev <morad.mlik@protonmail.com>
2023-10-01 18:51:00 +0200

    Premier test

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -4,3 +4,6 @@ class TestFizzBuzz (unittest.TestCase) :
 
     def setUp(self) :
         self.instance=FizzBuzz()
+
+    def test_affiche(self):
+        self.assertEqual(self.instance.affiche(), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz373839Buzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374Buzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz")
